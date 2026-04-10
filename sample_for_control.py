a92b6848b7a58b35321073e36e8dc3ae51269f11
Nihal <your_email@example.com>
2026-04-11 01:44:43 +0600

    updated controller code for the user

--- sample_for_control.py
+++ sample_for_control.py
@@ -1,12 +1,12 @@
 import paho.mqtt.client as mqtt
 
 broker = "broker.hivemq.com" 
-topic = "buet/cse/2105105/led" # TODO: Put the same topic you used in the ESP code
+topic = "buet/cse/2105105/led" # Put the same topic you used in the ESP code
 
 client = mqtt.Client()
 client.connect(broker)
 
-# TODO: the following is an example of publishing a message. You have to modify it so that the python code will run infinitely and wait for input from keyboard. If user presses 'y', it will send "ON"; it will send "OFF" if 'n' is pressed. The program will terminate if user presses 'q'.
+# this loop will run until the user decides to quit by pressing 'q' and will publish "ON" or "OFF" to the MQTT topic based on user input , which will control the LED connected to the ESP device subscribed to the same topic.(topic: buet/cse/2105105/led)
 while True:
 	key = input("Press y for ON, n for OFF, q to quit: ").strip().lower()
 	if key == "y":
@@ -21,4 +21,5 @@ while True:
 		print("Invalid input. Use y, n, or q.")
 
 client.disconnect()
+
 
